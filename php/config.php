ce9476579be66279ad7dc3c72b9216631a45e1de
Rohit Rehni <rohitrehni.rr@gmail.com>
2019-07-21 20:36:57 +0530

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,7 +1,7 @@
 <?php
    define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'joxbtxnk_joxbe_admin_sys');
-   define('DB_PASSWORD', '6~n$cjLRYyX(');
-   define('DB_DATABASE', 'joxbtxnk_wp_950');
+   define('DB_USERNAME', 'xxxxxxxxxxxxxx');
+   define('DB_PASSWORD', 'xxxxxxxxxx');
+   define('DB_DATABASE', 'xxxxxxxxxxx');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
